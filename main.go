a954a7e239d30f522e2c052c46d021206e984dc2
Philip Krause <pkrause@bparker-mojave.hci01.local>
2019-09-17 16:46:46 -0400

    Complete Crud Api

--- main.go
+++ main.go
@@ -6,17 +6,26 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
 func homeLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Magic Happening on Port 8080")
 	fmt.Fprintf(w, "GO API!")
 }
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
+
 	router.HandleFunc("/", homeLink)
+	router.HandleFunc("/events", getAllEvents).Methods("GET")
+	router.HandleFunc("/events/{id}", getOneEvent).Methods("GET")
+	router.HandleFunc("/events", createEvent).Methods("POST")
+	router.HandleFunc("/events/{id}", updateEvent).Methods("PATCH")
+	router.HandleFunc("/events/{id}", deleteEvent).Methods("DELETE")
+
 	log.Fatal(http.ListenAndServe(":8080", router))
 
 }
@@ -51,4 +60,71 @@ func createEvent(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 
 	json.NewEncoder(w).Encode(newEvent)
+}
+
+func getOneEvent(w http.ResponseWriter, r *http.Request) {
+	eventID := mux.Vars(r)["id"] //create map of all routes
+
+	//convert to int
+	eventIntID, err := strconv.Atoi(eventID)
+
+	if err != nil {
+		fmt.Println(eventIntID, "is not a int")
+	}
+
+	for _, singleEvent := range events {
+		if singleEvent.ID == eventIntID {
+			json.NewEncoder(w).Encode(singleEvent)
+		}
+	}
+}
+
+func getAllEvents(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(events)
+}
+
+func updateEvent(w http.ResponseWriter, r *http.Request) {
+	eventID := mux.Vars(r)["id"]
+
+	eventIntID, err := strconv.Atoi(eventID)
+
+	if err != nil {
+		fmt.Println(eventID, "is not an int")
+	}
+
+	var updatedEvent event
+
+	reqBody, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
+	}
+	json.Unmarshal(reqBody, &updatedEvent)
+
+	for i, singleEvent := range events {
+		if singleEvent.ID == eventIntID {
+			singleEvent.Title = updatedEvent.Title
+			singleEvent.Description = updatedEvent.Description
+			events = append(events[:i], singleEvent)
+			json.NewEncoder(w).Encode(singleEvent)
+
+		}
+	}
+}
+
+func deleteEvent(w http.ResponseWriter, r *http.Request) {
+	eventID := mux.Vars(r)["id"]
+
+	eventIntID, err := strconv.Atoi(eventID)
+
+	if err != nil {
+		fmt.Println(eventID, "must be an integer")
+	}
+
+	for i, singleEvent := range events {
+		if singleEvent.ID == eventIntID {
+			events = append(events[:i], events[i+1:]...)
+			fmt.Fprintf(w, "The event with ID %v has been deleted sucessfully", eventIntID)
+		}
+	}
 }
